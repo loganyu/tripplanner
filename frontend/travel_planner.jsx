09fc8b51dd41a2fe7953bd05e4acab30c646a4b5
Logan Yu <yu.logan@gmail.com>
2019-04-24 15:05:05 -0400

    update entry point to root

--- frontend/travel_planner.jsx
+++ frontend/travel_planner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -23,5 +24,5 @@ document.addEventListener('DOMContentLoaded', () => {
   window.dispatch = store.dispatch; // just for testing!
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Travel Planner</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
