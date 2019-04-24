1f99258a603ddd78e6d5b3bd331558479e211772
Logan Yu <yu.logan@gmail.com>
2019-04-24 14:29:48 -0400

    call configureStore and assign result to window

--- frontend/travel_planner.jsx
+++ frontend/travel_planner.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { id: window.currentUser.id },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  // we don't put the store directly on the window because
+  // it can be confusing when debugging, sometimes giving you access to state
+  // when you shouldn't
+  window.getState = store.getState;
+  window.dispatch = store.dispatch; // just for testing!
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Travel Planner</h1>, root);
 });
