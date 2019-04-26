3817c1a1c21c9910afea4786976470a35b3a085a
Logan Yu <yu.logan@gmail.com>
2019-04-26 17:32:38 +0100

    add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
 
   has_many :trips
 
+  class Roles
+    ADMIN = "admin"
+    MANAGER = "manager"
+  end
+
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil unless user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_040306) do
+ActiveRecord::Schema.define(version: 2019_04_26_162137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_04_25_040306) do
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
