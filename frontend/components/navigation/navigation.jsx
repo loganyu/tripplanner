3f02063217976b2e73a650cc3cc131a6803fb4db
Logan Yu <yu.logan@gmail.com>
2019-04-27 22:22:46 +0100

    add AppBar

--- frontend/components/navigation/navigation.jsx
+++ frontend/components/navigation/navigation.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';  
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+};
 
 class Navigation extends React.Component {
   constructor(props) {
@@ -20,28 +36,38 @@ class Navigation extends React.Component {
   }
 
   render() {
-    const { currentUser } = this.props;
+    const { currentUser, classes } = this.props;
 
     return (
-      <div>
-        {currentUser &&
-          <button color="inherit" onClick={this.handleLogOut}>
-            Log Out
-          </button>
-        }
-        {!currentUser &&
-          <div>
-            <button onClick={() => this.handleNavigate(`/login`)}>
-              Log In
-            </button>
-            <button onClick={() => this.handleNavigate(`/signup`)}>
-              Sign Up
-            </button>
-          </div>
-        }
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Travel Planner
+            </Typography>
+            <div>
+              {currentUser &&
+                <div>
+                  <Button color="inherit" onClick={() => this.handleNavigate(`/`)}>Home</Button>
+                  <Button color="inherit" onClick={this.handleLogOut}>Log Out</Button>
+                </div>
+              }
+              {!currentUser &&
+                <div>
+                  <Button color="inherit" onClick={() => this.handleNavigate(`/login`)}>Log In</Button>
+                  <Button color="inherit" onClick={() => this.handleNavigate(`/signup`)}>Sign Up</Button>
+                </div>
+              }
+            </div>
+          </Toolbar>
+        </AppBar>
       </div>
     )
   }
 };
 
-export default withRouter(Navigation);
+Navigation.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withRouter(connect()(withStyles(styles)(Navigation)));
