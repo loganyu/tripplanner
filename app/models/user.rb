546b5a3e9e2958fe46c012036ab54e656a1717fe
Logan Yu <yu.logan@gmail.com>
2019-04-27 21:39:05 +0100

    show user error if username already taken

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   validates :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :username, uniqueness: true
 
   after_initialize :ensure_session_token
 
