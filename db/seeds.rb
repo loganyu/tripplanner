fce8e7a5cfe314ab89832d252bcd19846b466b9e
Logan Yu <yu.logan@gmail.com>
2019-04-27 15:44:31 +0100

    add seed data generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,3 +38,18 @@ Trip.create!(
   :start_date => Date.new(2019,1,17),
   :end_date => Date.new(2019,2,16),
 )
+
+100.times do
+  username = Faker::Name.name.split.join('_')
+  user = User.create(username: username, password: 'password')
+  5.times do
+    start_date = Faker::Date.between(2.years.ago, 2.years.from_now)
+    Trip.create!(
+      :user_id => user.id,
+      :destination => Faker::Nation.capital_city,
+      :comment => Faker::Lorem.paragraph,
+      :start_date => start_date,
+      :end_date => Faker::Date.between(start_date, start_date + 2.years),
+    )
+  end
+end
