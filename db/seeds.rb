ce0bcbbf2b4eea0c6c21ec50b7a6649c9ddabf2d
Logan Yu <yu.logan@gmail.com>
2019-04-28 21:21:08 +0100

    modify seed data for more trips in the coming month

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,13 +43,13 @@ Trip.create!(
   username = Faker::Name.name.split.join('_')
   user = User.create(username: username, password: 'password', role: 'manager')
   10.times do
-    start_date = Faker::Date.between(2.years.ago, 2.years.from_now)
+    start_date = Faker::Date.between(1.months.ago, 2.months.from_now)
     Trip.create!(
       :user_id => user.id,
       :destination => Faker::Nation.capital_city,
       :comment => Faker::Lorem.paragraph,
       :start_date => start_date,
-      :end_date => Faker::Date.between(start_date, start_date + 2.years),
+      :end_date => Faker::Date.between(start_date + 2.days, start_date + 2.weeks),
     )
   end
 end
@@ -58,13 +58,13 @@ end
   username = Faker::Name.name.split.join('_')
   user = User.create(username: username, password: 'password', role: 'admin')
   10.times do
-    start_date = Faker::Date.between(2.years.ago, 2.years.from_now)
+    start_date = Faker::Date.between(1.months.ago, 2.months.from_now)
     Trip.create!(
       :user_id => user.id,
       :destination => Faker::Nation.capital_city,
       :comment => Faker::Lorem.paragraph,
       :start_date => start_date,
-      :end_date => Faker::Date.between(start_date, start_date + 2.years),
+      :end_date => Faker::Date.between(start_date + 2.days, start_date + 2.weeks),
     )
   end
 end
@@ -73,13 +73,13 @@ end
   username = Faker::Name.name.split.join('_')
   user = User.create(username: username, password: 'password')
   10.times do
-    start_date = Faker::Date.between(2.years.ago, 2.years.from_now)
+    start_date = Faker::Date.between(1.months.ago, 2.months.from_now)
     Trip.create!(
       :user_id => user.id,
       :destination => Faker::Nation.capital_city,
       :comment => Faker::Lorem.paragraph,
       :start_date => start_date,
-      :end_date => Faker::Date.between(start_date, start_date + 2.years),
+      :end_date => Faker::Date.between(start_date + 2.days, start_date + 2.weeks),
     )
   end
 end
